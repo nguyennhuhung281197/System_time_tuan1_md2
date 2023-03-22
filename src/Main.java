e482b286989bfb1b7bb0bc752a451b7e0980032f
nguyennhuhung281197 <nhuhung281197@gmail.com>
2023-03-22 16:15:21 +0700

    update

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Date now = new Date();
 
-        System.out.println("Now is " +  now);
+        System.out.println("Now is" +  now);
     }
 }
